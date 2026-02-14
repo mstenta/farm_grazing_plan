dc0681d967e2bad85a6f05fda9564864e13a0d01
Michael Stenta <mike@mstenta.net>
2026-02-14 12:48:57 -0500

    Do not inherit plan bundle fields.

--- src/Plugin/Plan/PlanType/Grazing.php
+++ src/Plugin/Plan/PlanType/Grazing.php
@@ -20,7 +20,7 @@ class Grazing extends FarmPlanType {
    * {@inheritdoc}
    */
   public function buildFieldDefinitions() {
-    $fields = parent::buildFieldDefinitions();
+    $fields = [];
     $field_info = [
       'season' => [
         'type' => 'entity_reference',
