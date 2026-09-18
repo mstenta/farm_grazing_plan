e0bfac59e39fae4d5f621f51988e281228e32d1f
Michael Stenta <mike@mstenta.net>
2026-09-17 20:26:27 -0400

    Split MockGrazingPlanEntitiesTrait::createMockGrazingEvent() out of ::createMockPlanEntities().

--- tests/src/Traits/MockGrazingPlanEntitiesTrait.php
+++ tests/src/Traits/MockGrazingPlanEntitiesTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\Tests\farm_grazing_plan\Traits;
 
 use Drupal\asset\Entity\Asset;
+use Drupal\asset\Entity\AssetInterface;
 use Drupal\log\Entity\Log;
 use Drupal\plan\Entity\Plan;
 use Drupal\plan\Entity\PlanInterface;
@@ -59,6 +60,13 @@ trait MockGrazingPlanEntitiesTrait {
   protected ?PlanInterface $plan = NULL;
 
   /**
+   * Grazing events.
+   *
+   * @var \Drupal\plan\Entity\PlanRecordInterface[]
+   */
+  protected array $grazingEvents = [];
+
+  /**
    * Create mock grazing plan entities.
    */
   public function createMockPlanEntities(): void {
@@ -102,30 +110,6 @@ trait MockGrazingPlanEntitiesTrait {
       $this->landAssets[] = $asset;
     }
 
-    // Start the plan on May 1.
-    $timestamp = strtotime('May 1, ' . $year);
-
-    // Create activity logs that move each animal through all paddocks.
-    foreach ($this->animalAssets as $animal_asset) {
-      foreach ($this->landAssets as $land_asset) {
-        $log = Log::create([
-          'name' => 'Move ' . $animal_asset->label() . ' to ' . $land_asset->label(),
-          'type' => 'activity',
-          'timestamp' => strtotime('+7 days', $timestamp),
-          'asset' => [
-            ['target_id' => $animal_asset->id()],
-          ],
-          'location' => [
-            ['target_id' => $land_asset->id()],
-          ],
-          'is_movement' => TRUE,
-          'status' => 'done',
-        ]);
-        $log->save();
-        $this->movementLogs[] = $log;
-      }
-    }
-
     // Create a grazing plan for the season.
     $this->plan = Plan::create([
       'name' => $this->seasonTerm->label() . ' Grazing Plan',
@@ -136,18 +120,61 @@ trait MockGrazingPlanEntitiesTrait {
     ]);
     $this->plan->save();
 
-    // Create grazing_event plan_record entities to link movement logs to the
-    // plan.
-    foreach ($this->movementLogs as $log) {
+    // Create activity logs that move each animal through all paddocks.
+    $timestamp = NULL;
+    foreach ($this->animalAssets as $animal_asset) {
+      foreach ($this->landAssets as $land_asset) {
+
+        // If this is the first log, start on May 1.
+        if (is_null($timestamp)) {
+          $timestamp = strtotime('May 1, ' . $year);
+        }
+
+        // Otherwise, add a week to the previous timestamp.
+        else {
+          $timestamp = strtotime('+7 days', $timestamp);
+        }
+
+        // Create the log and plan_record entities.
+        $this->createMockGrazingEvent($timestamp, $animal_asset, $land_asset, TRUE, $this->plan);
+      }
+    }
+  }
+
+  /**
+   * Create mock grazing event log (and optional plan_record) entity.
+   */
+  public function createMockGrazingEvent(int $timestamp, AssetInterface $asset, AssetInterface $location, bool $plan_record = TRUE, ?PlanInterface $plan = NULL) {
+
+    // Create the log entity.
+    $log = Log::create([
+      'name' => 'Move ' . $asset->label() . ' to ' . $location->label(),
+      'type' => 'activity',
+      'timestamp' => $timestamp,
+      'asset' => [
+        ['target_id' => $asset->id()],
+      ],
+      'location' => [
+        ['target_id' => $location->id()],
+      ],
+      'is_movement' => TRUE,
+      'status' => 'done',
+    ]);
+    $log->save();
+    $this->movementLogs[] = $log;
+
+    // Create a plan_record, if desired.
+    if ($plan_record) {
       $grazing_event = PlanRecord::create([
         'type' => 'grazing_event',
-        'plan' => ['target_id' => $this->plan->id()],
+        'plan' => ['target_id' => $plan->id()],
         'log' => ['target_id' => $log->id()],
         'start' => $log->get('timestamp')->value,
         'duration' => 7 * 24,
         'recovery' => 15 * 24,
       ]);
       $grazing_event->save();
+      $this->grazingEvents[] = $grazing_event;
     }
   }
 
